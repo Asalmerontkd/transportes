0a4a7e2db79ccab738317eb051caa7df74d05f8c
Asalmerontkd <psrantonio@gmail.com>
2017-03-08 11:54:06 -0600

    Error 500 fixed en la carga de archivos

--- clase/DatosTemp.php
+++ clase/DatosTemp.php
@@ -10,11 +10,17 @@
 		{
 			$fechaTemp = date("Y-m-d H:i:s");
 			$con = Database::getInstance();
-			$sql = "INSERT INTO temporal(user, fechaTemp, region, idCentro, centro, tarjeta, idVehiculo, descVehiculo, noComprobante, fecha, codPemex, razonSocial, cerCualli, estado, kmAntes, kmTransaccion, capacidad, mercancia, cantMercancia, precioUni, idTerminal, placa) VALUES (:user, :fechaTemp, :region, :idCentro, :centro, :tarjeta, :idVehiculo, :descVehiculo, :noComprobante, :fecha, :codPemex, :razonSocial, :cerCualli, :estado, :kmAntes, :kmTransaccion, :capacidad, :mercancia, :cantMercancia, :precioUni, :idTerminal, :placa)"
+			$sql = "INSERT INTO temporal(user, fechaTemp, region, idCentro, centro, tarjeta, idVehiculo, descVehiculo, noComprobante, fecha, codPemex, razonSocial, cerCualli, estado, kmAntes, kmTransaccion, capacidad, mercancia, cantMercancia, precioUni, idTerminal, placa) VALUES (:user, :fechaTemp, :region, :idCentro, :centro, :tarjeta, :idVehiculo, :descVehiculo, :noComprobante, :fecha, :codPemex, :razonSocial, :cerCualli, :estado, :kmAntes, :kmTransaccion, :capacidad, :mercancia, :cantMercancia, :precioUni, :idTerminal, :placa)";
 			$result = $con->db->prepare($sql);
 			$params = array("user" =>$user, "fechaTemp" => $fechaTemp, "region" => $region, "idCentro" => $idCentro, "centro" => $centro, "tarjeta" => $tarjeta, "idVehiculo" => $idVehiculo, "descVehiculo" => $descVehiculo, "noComprobante" => $noComprobante, "fecha" => $fecha, "codPemex" => $codPemex, "razonSocial" => $razonSocial, "cerCualli" => $cerCualli, "estado" => $estado, "kmAntes" => $kmAntes, "kmTransaccion" => $kmTransaccion, "capacidad" => $capacidad, "mercancia" => $mercancia, "cantMercancia" => $cantMercancia, "precioUni" => $precioUni, "idTerminal" => $idTerminal, "placa" => $placa);
 			$result->execute($params);
 			return ($result);
+		}
+
+		public function consultarTemporal($user)
+		{
+			$con = Database::getInstance();
+			$sql = "";
 		}
 	}
 ?>
